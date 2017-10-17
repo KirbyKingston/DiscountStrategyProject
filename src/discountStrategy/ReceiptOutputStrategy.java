a8c6b2fb7af934a5898a2deadf535d743d0468c2
Mitch <Mitch@Mitch-LAPTOP-Lenovo>
2017-10-17 14:13:02 -0500

    GUI work

--- src/discountStrategy/ReceiptOutputStrategy.java
+++ src/discountStrategy/ReceiptOutputStrategy.java
@@ -5,10 +5,11 @@ package discountStrategy;
  *
  * @author Mitch
  */
-public interface ReceiptOutputStrategy {
+public interface ReceiptOutputStrategy extends SaleDetailStrategy{
 
     public abstract void outputReceiptHeader(final int receiptId, final String customerId, final String customerName);
     
+    @Override
     public abstract void outputReceiptBody(final String receiptBodyContent);
     
     public abstract void outputReceiptFooter();
---
+++ src/discountStrategy/SaleDetailStrategy.java
@@ -0,0 +1,12 @@
+
+package discountStrategy;
+
+/**
+ *
+ * @author Mitch
+ */
+public interface SaleDetailStrategy {
+    
+    public abstract void outputReceiptBody(final String receiptBodyContent);
+    
+}
